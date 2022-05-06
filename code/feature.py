305e57d6a865ac5d81d0c8d1b84abdd3052e2de3
ShirleyPmy <44694304+ShirleyPmy@users.noreply.github.com>
2022-05-06 17:17:46 +0800

    Update feature.py

--- code/feature.py
+++ code/feature.py
@@ -25,12 +25,30 @@ def ball_position(data):
     df_model = data.df_model.copy()
     
     # 小数位数太多容易过拟合
-    df_model['ball_x'] = data.df['ball'].apply(lambda x:round(x[0],4))
-    df_model['ball_y'] = data.df['ball'].apply(lambda x:round(x[1],4))
-    df_model['ball_z'] = data.df['ball'].apply(lambda x:round(x[2],4))
+    df_model['ball_position_x'] = data.df['ball'].apply(lambda x:round(x[0],4))
+    df_model['ball_position_y'] = data.df['ball'].apply(lambda x:round(x[1],4))
+    df_model['ball_position_z'] = data.df['ball'].apply(lambda x:round(x[2],4))
     
     data.df_model = df_model
-    
+
+def ball_velocity(data):
+    df = data.df.copy()
+    df_model = data.df_model.copy()
+
+    # 小数位数太多容易过拟合
+    df_model['ball_velocity_x'] = data.df['ball_direction'].apply(lambda x: round(x[0], 4))
+    df_model['ball_velocity_y'] = data.df['ball_direction'].apply(lambda x: round(x[1], 4))
+    df_model['ball_velocity_z'] = data.df['ball_direction'].apply(lambda x: round(x[2], 4))
+
+    data.df_model = df_model
+
+def ball_control(data):
+    df = data.df.copy()
+    df_model = data.df_model.copy()
+
+    df_model['ball_control'] = df['ball_owned_team_new']
+    data.df_model = df_model
+
 def owned_player_position(data):
     df = data.df.copy()
     df_model = data.df_model.copy()
@@ -57,15 +75,41 @@ def owned_player_position(data):
     df_model['owned_player_position_y'] = owned_player_position.apply(lambda x:round(x[1],4))
     
     data.df_model = df_model
+
+def owned_player_velocity(data):
+    df = data.df.copy()
+    df_model = data.df_model.copy()
+
+    # 小数位数太多容易过拟合
+    df_model['owned_player_velocity_x'] = -1
+    df_model['owned_player_velocity_y'] = -1
+
+    def from_player_index_get_velocity(line):
+        team = line['ball_owned_team']
+        index = line['ball_owned_player']
+        if (team == 0):
+            position = line['left_team_direction']
+        elif (team == 1):
+            position = line['right_team_direction']
+        else:
+            return np.array([-10, 10])
+
+        return position[index]
+
+    owned_player_velocity = data.df.apply(from_player_index_get_velocity, axis=1)
+    df_model['owned_player_velocity_x'] = owned_player_velocity.apply(lambda x: round(x[0], 4))
+    df_model['owned_player_velocity_y'] = owned_player_velocity.apply(lambda x: round(x[1], 4))
+
+    data.df_model = df_model
     
 def owned_ball_distance_horizontal(data):
     df_model = data.df_model.copy()
     
-    #这里是偷懒的写法，因为直接使用df_model中的位置计算，而改位置只取了4位小数，再计算距离时不够精确
-    df_model['owned_ball_distance_horizontal'] = ((df_model['ball_x'] - df_model['owned_player_position_x'])**2 + (df_model['ball_y'] - df_model['owned_player_position_y'])**2)**0.5
+    #这里是偷懒的写法，因为直接使用df_model中的位置计算，而改位置我们只取了4位小数，再计算距离时不够精确
+    df_model['owned_ball_distance_horizontal'] = ((df_model['ball_position_x'] - df_model['owned_player_position_x'])**2 +
+                                                  (df_model['ball_position_y'] - df_model['owned_player_position_y'])**2)**0.5
     
     data.df_model = df_model
-    # 这里有一些思路， 比如我算了控球的队员离球的水平距离，那么其实也可以算一下离球最近的对方队员的距离，或许也对预测最终结果有帮助
 
 def dis_owned_left(data):
     df = data.df.copy()
@@ -124,16 +168,14 @@ def team_tired_factor_mean(data):
     df_model['right_team_tired_mean'] = df['right_team_tired_factor'].apply(lambda x:np.mean(x))
     
     data.df_model = df_model
-    
-    #比如这里可以做一些，控球球员的疲惫值，周围xx米内队友/对手的平均疲惫值，离你最近的3个队友/对手的平均疲惫值
-    
+
 
 def goal_position_mean(data):
     # 统计特征，统计上一场比赛的平均进球时（label=1），球的平均位置
     df = data.df.copy()
     df_model = data.df_model.copy()
     
-    position = df_model.loc[df_model['label']==1,['ball_x','ball_y','ball_z','match_num']]
+    position = df_model.loc[df_model['label']==1,['ball_position_x','ball_position_y','ball_position_z','match_num']]
     position = position.groupby('match_num').mean()
     position.columns = ['last_match_goal_position_mean_x','last_match_goal_position_mean_y','last_match_goal_position_mean_z']
     
@@ -147,16 +189,15 @@ def goal_position_mean(data):
     #类似的统计特征可以多搞搞，本场比赛的，其他比赛的，就是注意不要leak（标签泄漏）
 
 
-#说一点特征思路，正常指定要计算一些，球，球门，球员的位置信息，距离，速度，前方是不是有队友接应等，可以自己试试，能做的很多，前方的敌人数量等
-#但是比较tricky的思路的话，感觉可以把球场建模成一个矩阵，然后上面有球、人的位置，状态等，用比如cnn等方式抽取一些embedding表示，让模型自己学习上述特征
-
+#特征思路：正常指定要计算一些，球，球门，球员的位置信息，距离，速度，前方是不是有队友接应，前方的敌人数量等
+#比较tricky的思路的话，感觉可以把球场建模成一个矩阵，然后上面有球、人的位置，状态等，用比如cnn等方式抽取一些embedding表示，让模型自己学习上述特征
 
 
 
 def feature_engineering(data):
     # 做特征就写一个函数，然后把函数名字加到这里， 后续做实验发现这个特征没什么用，那就可以在列表中删除它，但是函数还可以留着
-    good_funcs = [ steps_left_bin, ball_position, owned_player_position, owned_ball_distance_horizontal,
-                  team_tired_factor_mean, goal_position_mean, dis_owned_left, dis_owned_right]
+    good_funcs = [ball_control, ball_position, ball_velocity, owned_player_position, goal_position_mean,
+                owned_player_velocity, dis_owned_left, dis_owned_right]
     
     
     origin_columns = data.df_model.columns
